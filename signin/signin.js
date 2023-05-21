f9c327b6e86378e2039ce1c616161cb8e0df960a
Pulkit-Garg-01 <113122278+Pulkit-Garg-01@users.noreply.github.com>
2023-05-21 14:55:51 +0530

    Add files via upload

---
+++ signin/signin.js
@@ -0,0 +1,23 @@
+document.getElementById("button").addEventListener("click",signIn);
+     function signIn(event){
+        event.preventDefault();
+        var email=document.getElementById("email").value;
+        var password=document.getElementById("password").value;
+        var data={
+            email: email,
+            password: password
+          };
+          fetch('https://reqres.in/api/login', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+              'Content-type': 'application/json',
+            },
+           
+          })  
+          .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch(error=>console.log("Cannot fetch data"));
+
+
+     }
